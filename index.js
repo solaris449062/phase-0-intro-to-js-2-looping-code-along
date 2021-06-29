7cf011ed43f587f49ee8cdf786a147aa996795cc
Sungoh Park <spark449062@gmail.com>
2021-06-28 20:33:53 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        let newMessage = "Thank you, " + names[i] + ", for the wonderful " + eventName + " gift!"; 
+        messages = [...messages, newMessage];
+    }
+    return messages;
+}
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+// console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"));
+// countDown(10)
